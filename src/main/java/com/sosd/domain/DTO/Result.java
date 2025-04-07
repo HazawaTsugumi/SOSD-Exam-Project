9adbd5d2c4b7c8aee251f236d568004c6b9af64f
应国浩 <14959926+ying-guohao@user.noreply.gitee.com>
2025-04-07 18:56:27 +0800

    添加了Token验证的代码

---
+++ src/main/java/com/sosd/domain/DTO/Result.java
@@ -0,0 +1,62 @@
+package com.sosd.domain.DTO;
+
+import lombok.Getter;
+
+/**
+ * 最终返回给前端的对象
+ * @author 应国浩
+ */
+@Getter
+public class Result {
+    
+    /**
+     * 响应代码，200表示成功，其他表示失败
+     */
+    private Integer code;
+
+    /**
+     * 信息，如果成功为 success 失败则必须指定内容
+     */
+    private String message;
+
+    /**
+     * 数据，如果成功返回所需数据，失败为null
+     */
+    private Object data;
+
+    /**
+     * 使用静态方法配置请求成功的响应数据
+     * @param data
+     * @return
+     */
+    public static Result success(Object data){
+        Result result = new Result();
+        result.code = 200;
+        result.message = "success";
+        result.data = data;
+        return result;
+    }
+
+    /**
+     * 使用静态方法配置请求失败的响应数据
+     * @param msg
+     * @param code
+     * @return
+     */
+    public static Result fail(String msg,Integer code){
+        Result result = new Result();
+        result.code = code;
+        result.message = msg;
+        result.data = null;
+        return result;
+    }
+
+    /**
+     * 使用重载静态方法配置请求失败的默认响应数据，即代码为-1的数据
+     * @param msg
+     * @return
+     */
+    public static Result fail(String msg){
+        return Result.fail(msg, -1);
+    }
+}
---
+++ src/main/java/com/sosd/utils/JwtUtil.java
@@ -0,0 +1,88 @@
+package com.sosd.utils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+/**
+ * 用于生成 JWT，判断 JWT 是否过期以及解析 JWT 的工具类
+ * @author 应国浩
+ */
+@Component
+public class JwtUtil {
+    
+    /**
+     * 从配置文件中获取密钥
+     */
+    @Value("${my.secret}")
+    private String key;
+
+    /**
+     * 根据用户信息生成 JWT
+     * @param userInfo 用户信息
+     * @param type token类型
+     * @return 生成的 JWT
+     */
+    public String generate(String userInfo,TokenType type){
+
+        //获取签名日期以及过期日期
+        Date issuedDate = new Date();
+        Date expiredDate = new Date(issuedDate.getTime() + type.getTime());
+
+        //生成默认的base64头内容
+        Map<String,Object> map = new HashMap<>(16);
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+
+        //最终生成 JWT
+        return JWT.create()
+            .withHeader(map)
+            .withIssuer("sosd")
+            .withIssuedAt(issuedDate)
+            .withExpiresAt(expiredDate)
+            .withClaim("userInfo", userInfo)
+            .sign(Algorithm.HMAC256(key));
+    }
+
+    /**
+     * 判断 JWT 是否非法或过期
+     * @param jwt
+     * @return
+     */
+    public boolean verify(String jwt) {
+        try {
+
+            // 创建对应的判断器并确认 JWT 是否非法或过期
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(key)).build();
+            jwtVerifier.verify(jwt);
+
+            //如果未抛出异常，则说明该 JWT 合法且未过期
+            return true;
+        }catch (Exception e) {
+
+            //如果抛出异常，则说明该 JWT 不合法或过期
+            //我们不关心抛出的是什么异常，只需要知道该 JWT 过期即可
+            return false;
+        }
+    }
+
+    /**
+     * 解码 JWT 并获取用户信息，用于从token获取用户信息
+     * @param jwt
+     * @return
+     */
+    public String getUserInfo(String jwt) {
+
+        //解码 JWT 并获取用户信息
+        DecodedJWT decodedJWT = JWT.decode(jwt);
+        return decodedJWT.getClaim("userInfo").asString();
+    }
+}
