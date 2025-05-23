7f1391f6f0e462e74864e5c79fe3027e344be761
ljkaaaa <2849173390@qq.com>
2025-05-23 21:08:46 +0800

    1
    1

--- src/main/java/com/sosd/mapper/ImageBlogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sosd.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sosd.domain.POJO.ImageBlog;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ImageBlogMapper extends BaseMapper<ImageBlog> {
-}
